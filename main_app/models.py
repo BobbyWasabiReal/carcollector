be45f1b7bff0ce0142b724eed91611429deb3716
BobbyWasabiReal <wasabibob13@gmail.com>
2022-11-15 16:08:15 -0800

    Pt.2 complete

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Car(models.Model):
+    make = models.CharField(max_length=100)
+    model = models.CharField(max_length=200)
+    year = models.IntegerField()
+    description = models.TextField(max_length=250)
+
+    def __str__(self):
+        return f"{self.make} {self.model} ({self.id})"
--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home,  name = 'home'),
     path('about/', views.about,  name = 'about'),
     path('cars/', views.cars_index,  name = 'index'),
+    path('cars/<int:car_id>/', views.cars_detail, name = 'detail'),
 ]
